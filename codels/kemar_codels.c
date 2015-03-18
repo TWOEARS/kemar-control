58d09308469d7d7835519296a547839e7feea889
Ariel Podlubne <ariel.podlubne@laas.fr>
2015-03-18 14:47:06 +0100

    Changed activity to function to get the current position of the head (with timestamp)

--- codels/kemar_codels.c
+++ codels/kemar_codels.c
@@ -30,3 +30,27 @@
 #include "ackemar.h"
 
 #include "kemar_c_types.h"
+
+#include "includes.h"
+/* --- Function GetPosition --------------------------------------------- */
+
+/** Codel getPosition of function GetPosition.
+ *
+ * Returns genom_ok.
+ */
+genom_event
+getPosition(kemar_head *Head, genom_context self)
+{
+    if(h->homePos == true)
+    {
+        kemarGetInfo(h, k);
+        Head->position = k->posGearRad[0]*(180/pi);
+        Head->speed = k->velGearRadS*(180/pi);
+        Head->maxLeft = h->driveParam.leftRadMax*(180/pi);        
+        Head->maxRight = h->driveParam.rightRadMax*(180/pi);
+        gettimeofday(&tv, NULL);
+		Head->time.sec = tv.tv_sec;
+		Head->time.usec = tv.tv_usec;
+    }
+    return genom_ok;
+}
